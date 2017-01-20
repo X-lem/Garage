17b90d0343d57ccb69ce350b6eabbaf4d2b73124
Regis <X-lem@hotmail.com>
2017-01-20 13:10:04 -0800

    Update

---
+++ Garage/Garage/Garage.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Garage {
+    public partial class Garage : System.Web.UI.Page {
+
+        Panel panel;
+
+        protected void Page_Load(object sender, EventArgs e) {
+
+
+            panel = new Panel();
+            panel.CssClass = "Border";
+            this.Controls.Add(panel);
+
+        }
+    }
+}
---
+++ Garage/Garage/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Garage.Startup))]
+namespace Garage
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
